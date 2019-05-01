852d8348f6093e02a9930f52be694e0757a3a0be
lijinliang <13302059251@163.com>
2019-05-01 12:11:13 +0800

    First commit.

---
+++ app/src/main/java/com/coolweather/android/db/City.java
@@ -0,0 +1,49 @@
+package com.coolweather.android.db;
+
+import org.litepal.crud.DataSupport;
+
+/*
+ * 存放市数据的实体类
+ */
+public class City extends DataSupport {
+
+    private int id;
+    //记录城市的名字
+    private String cityName;
+    //记录城市的代号
+    private int cityCode;
+    //记录当前市所属省的id值
+    private int provinceId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
---
+++ app/src/main/java/com/coolweather/android/db/Province.java
@@ -0,0 +1,41 @@
+package com.coolweather.android.db;
+
+import org.litepal.crud.DataSupport;
+
+
+/*
+ * 存放省数据的实体类
+ */
+public class Province extends DataSupport {
+
+    //唯一标识
+    private int id;
+    //记录省的名字
+    private String provinceName;
+    //记录省的代号
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
